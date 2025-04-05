1b169c468023523b1021500ebeeded568283b74e
anino secret <bryanlomerioaninogithub@gmail.com>
2025-04-05 21:43:06 +0800

    chore: add .env.local

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,3 @@
-
 /// <reference types="vite/client" />
 
 interface Window {
@@ -10,3 +9,11 @@ declare module '@vapi-ai/web' {
   export function createVAPI(apiKey: string): any;
 }
 
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
